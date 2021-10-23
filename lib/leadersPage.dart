17576e6d8b135ac1d3096ed4e1b1bd2b56e99c11
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-10-23 22:49:33 +0200

    add leader file

---
+++ lib/leadersPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class leaderPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "leader",
+          style: TextStyle(fontSize: 30, color: Colors.black87),
+        ),
+      ),
+    );
+  }
+}
