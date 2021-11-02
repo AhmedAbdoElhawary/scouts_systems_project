160556dd3213f6875f12feccab9e8d96cb1b0cfa
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-03 00:48:56 +0200

    handling some widget and refactoring some widget

--- lib/view/seasons/eventsSeasonList.dart
+++ lib/view/seasons/eventsSeasonList.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/src/provider.dart';
+import 'package:scouts_system/common%20UI/CustomContainerBody.dart';
+import 'package:scouts_system/common%20UI/CustomWidgetMethods.dart';
+import 'package:scouts_system/common%20UI/showTheTextMessage.dart';
 import 'package:scouts_system/view%20model/eventsGetDataFirestore.dart';
 import 'package:scouts_system/view%20model/seasonsGetDataFirestore.dart';
 
@@ -21,93 +24,25 @@ class EventsSeasonList extends StatelessWidget {
         IndexesOfEventsData.add(i);
     }
     return Scaffold(
-        appBar: AppBar(),
-        body: ListView.separated(
-          itemCount: IndexesOfEventsData.length,
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: buildTheItemOfTheList(
-                  listOfAllEvents[IndexesOfEventsData[index]],
-                  index,
-                  listOfAllEvents[IndexesOfEventsData[index]]["docId"]),
-            );
-          },
-        ));
+        appBar: AppBar(backgroundColor: customColor()),
+        body: listOfEventsInSeason.length == 0
+            ? buildShowMessage("event")
+            : ListView.separated(
+                itemCount: IndexesOfEventsData.length,
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: buildTheItemOfTheList(
+                        listOfAllEvents[IndexesOfEventsData[index]], index),
+                  );
+                },
+              ));
   }
 
-  SafeArea buildTheItemOfTheList(var model, int index, String id) {
+  SafeArea buildTheItemOfTheList(var model, int index) {
     return SafeArea(
-      child: buildContainer(model, index),
-    );
-  }
-
-  Container buildContainer(var model, int index) {
-    return Container(
-      width: double.infinity,
-      child: Row(
-        children: [
-          buildCircleAvatarNumber(model, index),
-          buildColumnOfNameDescription(model),
-          buildColumnOfDateAndHours(model),
-        ],
-      ),
-    );
-  }
-
-  Column buildColumnOfDateAndHours(var model) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        buildText(model, "date"),
-      ],
-    );
-  }
-
-  Text buildText(var model, String text) {
-    return Text(
-      "${model[text]}",
-      style: text != "id"
-          ? TextStyle(
-              fontSize: 16,
-              color: Colors.black54,
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.italic)
-          : TextStyle(fontSize: 20, color: Colors.black),
-    );
-  }
-
-  Expanded buildColumnOfNameDescription(var model) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            buildText(model, "id"),
-            buildText(model, "location"),
-            buildText(model, "leader"),
-          ],
-        ),
-      ),
-    );
-  }
-
-  CircleAvatar buildCircleAvatarNumber(var model, int index) {
-    return CircleAvatar(
-      radius: 25,
-      backgroundColor: Colors.blue,
-      child: ClipOval(
-        child: Text(
-          "${index + 1}",
-          style: TextStyle(fontSize: 25, color: Colors.white),
-        ),
-      ),
+      child: CustomContainerBody(model: model, index: index, text: "event"),
     );
   }
 }
