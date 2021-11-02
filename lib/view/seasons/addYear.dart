af8ba45c3e188a9ee905de2e5a35eb79fe6cc035
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-03 00:47:02 +0200

    extract some widget

--- lib/view/seasons/addYear.dart
+++ lib/view/seasons/addYear.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:scouts_system/common%20UI/showToast.dart';
 import 'package:scouts_system/model/add%20data%20firestore/addFirestoreSeasons.dart';
+import 'package:scouts_system/common%20UI/CustomWidgetMethods.dart';
 
 class AddYear extends StatefulWidget {
   @override
@@ -13,65 +15,81 @@ class _AddYearState extends State<AddYear> {
 
   List<String> seasonsList = ["winter", "summer", "autumn", "spring"];
 
-  String seasonSelected = "summer";
+  String seasonSelected = "winter";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(children: [
-              Expanded(
-                child: TextFormField(
-                  controller: _controller,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "year",
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "year",
+                    ),
                   ),
                 ),
-              ),
-              DropdownButtonHideUnderline(
-                child: DropdownButton<String>(
-                  value:seasonSelected,
-                  isExpanded: false,
-                  icon: const Icon(Icons.keyboard_arrow_down_outlined),
-                  iconSize: 20,
-                  elevation: 16,
-                  style: const TextStyle(
-                      color: Colors.black, fontSize: 17, fontWeight: FontWeight.w300),
-                  onChanged: (n) {
-                    setState(() {
-                      seasonSelected = n!;
-                    });
-                  },
-                  items: buildListMap(seasonsList).toList(),
+                SizedBox(width: 10),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton<String>(
+                    value: seasonSelected,
+                    isExpanded: false,
+                    icon: const Icon(Icons.keyboard_arrow_down_outlined),
+                    iconSize: 20,
+                    elevation: 16,
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300),
+                    onChanged: (n) {
+                      setState(() {
+                        seasonSelected = n!;
+                      });
+                    },
+                    items: buildListMap(seasonsList).toList(),
+                  ),
                 ),
-              ),
-            ]),
-            Container(
-              color: Colors.blue,
-              child: TextButton(
-                  onPressed:(){
-                    WidgetsBinding.instance!.addPostFrameCallback((_) {
-                      if (_controller.text != "") {
-                        addFirestoreSeasons().addDataFirestoreSeasons(year:_controller.text ,season:seasonSelected );
-                      }
-                        Navigator.pop(context);
-                    });
-                  },
-                  child: Text(
-                    "   add   ",
-                    style: TextStyle(fontSize: 20, color: Colors.white),
-                  )),
-            )
-          ],
+              ]),
+              SizedBox(height: 15),
+              buildAddTextButton(context)
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Container buildAddTextButton(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: customColor(), borderRadius: BorderRadius.circular(10)),
+      child: TextButton(
+          onPressed: () {
+            WidgetsBinding.instance!.addPostFrameCallback((_) {
+              if (_controller.text != "") {
+                addFirestoreSeasons().addDataFirestoreSeasons(
+                    year: _controller.text, season: seasonSelected);
+                Navigator.pop(context);
+              } else {
+                ToastShow().showRedToast("Write something !");
+              }
+            });
+          },
+          child: Text(
+            "   add   ",
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          )),
+    );
+  }
+
   Iterable<DropdownMenuItem<String>> buildListMap(List<String> list) {
     return list.map<DropdownMenuItem<String>>((String value) {
       return DropdownMenuItem<String>(
