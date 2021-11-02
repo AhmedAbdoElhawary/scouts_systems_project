c3d8c5bcdd7f12666c0d1bf0107927dda1b7bbef
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-02 14:29:36 +0200

    make show toast more general

---
+++ lib/common UI/showToast.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ToastShow{
+  Future<bool?> showWhiteToast(var toast) {
+    return Fluttertoast.showToast(
+        msg: toast,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.white,
+        textColor: Colors.black);
+  }
+  Future<bool?> showRedToast(var toast) {
+    return Fluttertoast.showToast(
+        msg: toast,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white);
+  }
+}
