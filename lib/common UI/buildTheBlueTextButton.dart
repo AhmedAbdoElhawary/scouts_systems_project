a5370058298199c9914d6c37c7a3efe6fa5cb970
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-03 00:47:31 +0200

    extract some widget

--- lib/common UI/buildTheBlueTextButton.dart
+++ lib/common UI/buildTheBlueTextButton.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
 
-class BuildTextButton {
-  Container buildTheBlueTextButton(
-      {required String text,
-        required moveToPage,
-        required bool pop,
-        required BuildContext context,}) {
+class BuildBlueTextButton extends StatelessWidget {
+  String text;
+  var moveToPage;
+  bool pop;
+  BuildBlueTextButton(
+      {required this.text, required this.moveToPage, required this.pop});
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      color: Colors.blue,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 103, 135, 1.0),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 3,
+            blurRadius: 6,
+            offset: Offset(0, 5), // changes position of shadow
+          ),
+        ],
+      ),
       child: TextButton(
-          onPressed:(){
+          onPressed: () {
             WidgetsBinding.instance!.addPostFrameCallback((_) {
-              if(pop){
+              if (pop) {
                 Navigator.pop(context);
-              }else{
+              } else {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => moveToPage),
