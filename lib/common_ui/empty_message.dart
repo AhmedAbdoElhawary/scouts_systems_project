31b50e5a104f6057c3f7bbec4051f696221e08e4
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-07 01:34:22 +0200

    change the name of file from common UI to common_ui

---
+++ lib/common_ui/empty_message.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+Center emptyMessage(String text) {
+  return Center(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "There's no $text(s).",
+          style: TextStyle(fontSize: 15, color: Colors.black),
+        ),
+        Text(
+          "Try to add one !",
+          style: TextStyle(fontSize: 15, color: Colors.black),
+        ),
+      ],
+    ),
+  );
+}
