332d41064dc50e6e0522ccba0d6d8022a81740f0
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-07 01:44:10 +0200

    change the way of fetching data and extract som methods

--- lib/view/students/memberships_screen.dart
+++ lib/view/students/memberships_screen.dart
@@ -1,129 +1,118 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/src/provider.dart';
-import 'package:scouts_system/common%20UI/showTheTextMessage.dart';
-import 'package:scouts_system/view%20model/seasonsGetDataFirestore.dart';
-import 'package:scouts_system/view%20model/studentsGetDataFirestore.dart';
-import 'package:scouts_system/common%20UI/CustomWidgetMethods.dart';
-import 'package:scouts_system/view/students/membershipsCheckListPage.dart';
+import 'package:scouts_system/common_ui/circular_progress.dart';
+import 'package:scouts_system/common_ui/empty_message.dart';
+import 'package:scouts_system/view_model/seasons.dart';
+import 'memberships_check_list_screen.dart';
 
-class ListOfMembershipsStudent extends StatelessWidget {
-  String studentId;
-  ListOfMembershipsStudent(this.studentId);
+class MembershipsOfStudent extends StatelessWidget {
+  String studentDocId;
+  MembershipsOfStudent(this.studentDocId);
 
   Widget build(BuildContext context) {
-    context
-        .read<StudentsGetDataFirestore>()
-        .getStudentMembershipsData(studentId);
-
-    List<dynamic> listOfMemberships =
-        context
-            .watch<StudentsGetDataFirestore>()
-            .StudentMembershipsListOfData;
-
-    List<dynamic> listOfAllSeasons =
-        context
-            .watch<SeasonsGetDataFirestore>()
-            .seasonsListOfAllData;
-
-    List<int> SpecificIndexesOfSeasonsData = [];
-
-    for (int i = 0; i < listOfAllSeasons.length; i++) {
-      if (listOfMemberships.contains(listOfAllSeasons[i]["docId"]))
-        SpecificIndexesOfSeasonsData.add(i);
+    SeasonsLogic provider = context.watch<SeasonsLogic>();
+    if (provider.studentMemberships.isEmpty &&
+        provider.stateOfFetching != StateOfMemberships.loaded) {
+      provider.preparingMemberships(studentDocId);
+      return CircularProgress();
+    } else {
+      return buildScaffold(context, provider);
     }
+  }
 
+  Scaffold buildScaffold(BuildContext context, SeasonsLogic provider) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: customColor()),
-      body: SpecificIndexesOfSeasonsData.length == 0
-          ? buildShowMessage("memberships")
-          : ListView.separated(
-        itemCount: SpecificIndexesOfSeasonsData.length,
-        separatorBuilder: (BuildContext context, int index) =>
-        const Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: buildTheItemOfTheList(
-                listOfAllSeasons[SpecificIndexesOfSeasonsData[index]],
-                index),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: customColor(),
-        onPressed: () {
-          WidgetsBinding.instance!.addPostFrameCallback((_) {
-            List<QueryDocumentSnapshot<Object?>> listOfMemberships =
-                context.watch<SeasonsGetDataFirestore>().seasonsListOfAllData;
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        StudentCheckBoxMemberships(studentId,listOfMemberships)));
-          });
-        },
-        child: Icon(Icons.add),
-      ),
+      appBar: AppBar(),
+      body: provider.studentMemberships.isEmpty
+          ? emptyMessage("memberships")
+          : buildListView(provider),
+      floatingActionButton: buildFloatingActionButton(context),
     );
   }
 
-  SafeArea buildTheItemOfTheList(data, int index) {
+  ListView buildListView(SeasonsLogic provider) {
+    return ListView.separated(
+      itemCount: provider.studentMemberships.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: buildTheItemOfTheList(provider.studentMemberships[index], index),
+        );
+      },
+    );
+  }
+
+  FloatingActionButton buildFloatingActionButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () => onPressedFloating(context),
+      child: Icon(Icons.add),
+    );
+  }
+
+  onPressedFloating(BuildContext context) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => StudentCheckBoxMemberships(studentDocId)));
+  }
+
+  SafeArea buildTheItemOfTheList(Memberships membership, int index) {
     return SafeArea(
       child: InkWell(
         onTap: () async {},
-        child: buildContainer(data, index),
+        child: buildContainer(membership, index),
       ),
     );
   }
 
-  Container buildContainer(data, int index) {
+  Container buildContainer(Memberships membership, int index) {
     return Container(
       width: double.infinity,
       child: Row(
         children: [
           buildCircleAvatarNumber(index),
-          buildColumnOfNameDescription(data),
+          buildColumn(membership),
         ],
       ),
     );
   }
 
-  Expanded buildColumnOfNameDescription(data) {
+  Expanded buildColumn(Memberships membership) {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.only(left: 8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            buildText(data, "year"),
-            buildText(data, "season"),
-          ],
-        ),
+        child: buildColumnOfTexts(membership),
       ),
     );
   }
 
-  Text buildText(data, String text) {
+  Column buildColumnOfTexts(Memberships membership) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        buildText(membership.year),
+        buildText(membership.seasonType),
+      ],
+    );
+  }
+
+  Text buildText(String text) {
     return Text(
-      "${data[text]}",
-      style: text != "year"
-          ? TextStyle(
-              fontSize: 16,
-              color: Colors.black54,
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.italic)
-          : TextStyle(fontSize: 20, color: Colors.black),
+      text,
+      style: TextStyle(
+          fontSize: 16,
+          color: Colors.black54,
+          fontWeight: FontWeight.w500,
+          fontStyle: FontStyle.italic),
     );
   }
 
   CircleAvatar buildCircleAvatarNumber(int index) {
     return CircleAvatar(
       radius: 25,
-      backgroundColor: customColor(),
       child: ClipOval(
         child: Text(
           "${index + 1}",
