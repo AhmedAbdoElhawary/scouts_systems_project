664e670da2763e2d6fa5e9c3771221ced96baa3d
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-07 01:51:06 +0200

    put CustomContainerBody in the class to handling some error

--- lib/view/events/events_screen.dart
+++ lib/view/events/events_screen.dart
@@ -1,92 +1,163 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:scouts_system/common%20UI/CustomContainerBody.dart';
-import 'package:scouts_system/common%20UI/moveToThePage.dart';
-import 'package:scouts_system/common%20UI/showTheTextMessage.dart';
-import 'package:scouts_system/view%20model/eventsGetDataFirestore.dart';
-import 'package:scouts_system/view%20model/seasonsGetDataFirestore.dart';
-import 'package:scouts_system/common%20UI/CustomWidgetMethods.dart';
+import 'package:scouts_system/common_ui/circular_progress.dart';
+import 'package:scouts_system/common_ui/empty_message.dart';
+import 'package:scouts_system/common_ui/move_to_page.dart';
+import 'package:scouts_system/view_model/events.dart';
+import 'package:scouts_system/view_model/seasons.dart';
+import 'package:scouts_system/view_model/students.dart';
 
 class EventsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    context.read<EventsGetDataFirestore>().getAllEventsData();
+    EventsLogic provider = context.watch<EventsLogic>();
 
-    List<QueryDocumentSnapshot> listOfEventsData =
-        context.watch<EventsGetDataFirestore>().eventsListOfData;
+    if (provider.eventsList.isEmpty &&
+        provider.stateOfFetching != StateOfEvents.loading) {
+      provider.preparingEvents();
+      context.read<SeasonsLogic>().preparingSeasons();
+      return CircularProgress();
+    } else
+      return buildScaffold(context, provider);
+  }
 
-    Map<String, List<String>> listOfSeasonsData =
-        context.watch<SeasonsGetDataFirestore>().listOfSeasons;
-
-    Iterable<String> a = listOfSeasonsData.keys;
-
-    List<String> listOfYearsDropButton = List<String>.generate(
-        listOfSeasonsData.length, (i) => "${a.elementAt(i)}");
+  Scaffold buildScaffold(BuildContext context, EventsLogic provider) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
-        child: listOfEventsData.length == 0
-            ? buildShowMessage("event")
-            : ListView.separated(
-                itemCount: listOfEventsData.length,
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(),
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: buildTheItemOfTheList(
-                        listOfEventsData[index],
-                        index,
-                        listOfEventsData[index]["docId"],
-                        context,
-                        listOfYearsDropButton,
-                        listOfSeasonsData),
-                  );
-                },
-              ),
+        child: provider.eventsList.isEmpty
+            ? emptyMessage("event")
+            : buildListView(provider),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: customColor(),
-        onPressed: () async {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MoveToThePage().moveToEventInfo(
-                      "",
-                      "",
-                      false,
-                      context,
-                      listOfYearsDropButton,
-                      listOfSeasonsData)));
+      floatingActionButton: buildFloatingActionButton(context),
+    );
+  }
+
+  FloatingActionButton buildFloatingActionButton(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () async {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => MoveToThePage().moveToEventInfo(
+                    context: context,
+                    model: Events(leader: "leader 1"),
+                    seasonsFormat: [])));
+      },
+      child: Icon(Icons.add),
+    );
+  }
+
+  ListView buildListView(EventsLogic provider) {
+    return ListView.separated(
+      itemCount: provider.eventsList.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return buildListTile(provider, index, context);
+      },
+    );
+  }
+
+  ListTile buildListTile(
+      EventsLogic provider, int index, BuildContext context) {
+    return ListTile(
+        title: buildTheItemOfTheList(provider.eventsList[index], index,
+            provider.eventsList[index].eventDocId, context));
+  }
+
+  SafeArea buildTheItemOfTheList(
+      Events model, int index, String eventDocId, BuildContext context) {
+    return SafeArea(
+      child: InkWell(
+        onTap: () {
+          //to clear the previous data
+          context.read<StudentsLogic>().selectedStudentsCleared();
+          context.read<StudentsLogic>().remainingStudentsCleared();
+          context.read<StudentsLogic>().stateOfSelectedFetching =
+              StateOfSelectedStudents.initial;
+          //------------------------->
+
+          buildPush(context, model, eventDocId);
         },
-        child: Icon(Icons.add),
+        child: container(index, model),
       ),
     );
   }
 
-  SafeArea buildTheItemOfTheList(
-      var model,
-      int index,
-      String eventDocId,
-      context,
-      List<String> listOfYearsDropButton,
-      Map<String, List<String>> listOfSeasonsData) {
-    return SafeArea(
-      child: InkWell(
-        onTap: () async {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MoveToThePage().moveToEventInfo(
-                      model,
-                      eventDocId,
-                      true,
-                      context,
-                      listOfYearsDropButton,
-                      listOfSeasonsData)));
-        },
-        child: CustomContainerBody(model: model, text: "event", index: index),
+  buildPush(BuildContext context, Events model, String eventDocId) {
+    return Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MoveToThePage().moveToEventInfo(
+                model: model,
+                eventDocId: eventDocId,
+                checkForUpdate: true,
+                seasonsFormat: context.read<SeasonsLogic>().seasonsOfDropButton,
+                context: context)));
+  }
+
+  container(int index, Events model) {
+    return Container(
+      width: double.infinity,
+      child: Row(
+        children: [
+          buildCircleAvatarNumber(index),
+          buildNameAndDescription(model),
+          buildDateAndHours(model),
+        ],
+      ),
+    );
+  }
+
+  Column buildDateAndHours(Events model) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        buildText(model.date),
+        buildText(model.leader),
+      ],
+    );
+  }
+
+  Text buildText(String text) {
+    return Text(
+      text,
+      style: TextStyle(
+          fontSize: 16,
+          color: Colors.black54,
+          fontWeight: FontWeight.w500,
+          fontStyle: FontStyle.italic),
+    );
+  }
+
+  Expanded buildNameAndDescription(Events model) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            buildText(model.eventId),
+            buildText(model.location),
+          ],
+        ),
+      ),
+    );
+  }
+
+  CircleAvatar buildCircleAvatarNumber(int index) {
+    return CircleAvatar(
+      radius: 25,
+      child: ClipOval(
+        child: Text(
+          "${index + 1}",
+          style: TextStyle(fontSize: 25, color: Colors.white),
+        ),
       ),
     );
   }
