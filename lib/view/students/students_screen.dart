59bf64fc738495891b7516747f97d7f89ac39d2e
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-07 01:40:12 +0200

    Make the code more readable and handling the way of fetching data.

--- lib/view/students/students_screen.dart
+++ lib/view/students/students_screen.dart
@@ -1,65 +1,94 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:provider/provider.dart';
-import 'package:scouts_system/common%20UI/CustomContainerBody.dart';
-import 'package:scouts_system/common%20UI/moveToThePage.dart';
-import 'package:scouts_system/common%20UI/showTheTextMessage.dart';
-import 'package:scouts_system/view%20model/studentsGetDataFirestore.dart';
-import 'package:scouts_system/common%20UI/CustomWidgetMethods.dart';
+import 'package:provider/src/provider.dart';
+import 'package:scouts_system/common_ui/circular_progress.dart';
+import 'package:scouts_system/common_ui/custom_container.dart';
+import 'package:scouts_system/common_ui/empty_message.dart';
+import 'package:scouts_system/view_model/seasons.dart';
+import 'package:scouts_system/view_model/students.dart';
+import 'add_student_Item.dart';
 
-class StudentPage extends StatelessWidget {
+class StudentsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    context.read<StudentsGetDataFirestore>().getAllStudentsData();
+    StudentsLogic provider = context.watch<StudentsLogic>();
+    if (provider.studentsList.isEmpty &&
+        provider.stateOfFetching != StateOfStudents.loading) {
+      provider.preparingStudents();
+      return CircularProgress();
+    } else {
+      return buildScaffold(context, provider);
+    }
+  }
 
-    List<QueryDocumentSnapshot> listOfStudentsData =
-        context.watch<StudentsGetDataFirestore>().StudentsListOfData;
-
+  Scaffold buildScaffold(BuildContext context, StudentsLogic provider) {
     return Scaffold(
-      body: SafeArea(
-        child: listOfStudentsData.length == 0
-            ? buildShowMessage("student")
-            : ListView.separated(
-                itemCount: listOfStudentsData.length,
-                separatorBuilder: (BuildContext context, int index) =>
-                    const Divider(),
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: buildTheItemOfTheList(listOfStudentsData[index],
-                        index, listOfStudentsData[index]["docId"], context),
-                  );
-                },
-              ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor:customColor(),
-      onPressed: () {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) =>
-                      MoveToThePage().moveToNewStudent("", "", false)));
-        },
-        child: Icon(Icons.add),
-      ),
+      appBar: AppBar(),
+      body: provider.studentsList.isEmpty
+          ? emptyMessage("students")
+          : buildListView(context, provider.studentsList),
+      floatingActionButton: buildFloatingActionButton(context),
     );
   }
 
-  SafeArea buildTheItemOfTheList(
-      QueryDocumentSnapshot model, int index, String studentDocId, context) {
-    return SafeArea(
-      child: InkWell(
-        onTap: () async {
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => MoveToThePage()
-                      .moveToNewStudent(model, studentDocId, true)));
-        },
-        child: CustomContainerBody(text: "student", index: index, model: model),
-      ),
+  ListView buildListView(BuildContext context, List<Students> studentsList) {
+    return ListView.separated(
+      itemCount: studentsList.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: buildStudentItem(studentsList[index], index, context),
+        );
+      },
     );
+  }
+
+  FloatingActionButton buildFloatingActionButton(BuildContext context) {
+    return FloatingActionButton(
+        onPressed: () => onPressedFloating(context), child: Icon(Icons.add));
+  }
+
+  onPressedFloating(BuildContext context) {
+    return Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => studentInformationScreen(
+                //default value(i can't make them with the constructor)
+                // -------------------------------------->
+                controllerOfName: TextEditingController(text: ""),
+                controllerOfDescription: TextEditingController(text: ""),
+                controllerOfBirthdate: TextEditingController(text: ""),
+                controllerOfHours: TextEditingController(text: ""))));
+    // --------------------------------------->
+  }
+
+  InkWell buildStudentItem(
+      Students studentInfo, int index, BuildContext context) {
+    return InkWell(
+      onTap: () => onTapItem(context, studentInfo),
+      child: CustomContainer(index: index, model: studentInfo),
+    );
+  }
+
+  onTapItem(BuildContext context, Students studentInfo) {
+    //clear lists from previous data
+    context.read<SeasonsLogic>().membershipsListCleared();
+    context.read<SeasonsLogic>().seasonsListCleared();
+    context.read<SeasonsLogic>().remainingMembershipsCleared();
+    //------------------------------->
+    return Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => studentInformationScreen(
+                controllerOfName: TextEditingController(text: studentInfo.name),
+                controllerOfDescription:
+                    TextEditingController(text: studentInfo.description),
+                controllerOfBirthdate:
+                    TextEditingController(text: studentInfo.birthdate),
+                controllerOfHours:
+                    TextEditingController(text: studentInfo.volunteeringHours),
+                studentDocId: studentInfo.docId,
+                checkForUpdate: true)));
   }
 }
