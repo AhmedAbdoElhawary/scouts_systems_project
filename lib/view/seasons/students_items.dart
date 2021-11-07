77d8117e69bda2872c860e5b5bd08a6f2c8b7a9a
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-07 15:30:04 +0200

    change the way of fetching data and extract some methods

--- lib/view/seasons/students_items.dart
+++ lib/view/seasons/students_items.dart
@@ -2,55 +2,54 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/src/provider.dart';
-import 'package:scouts_system/common%20UI/CustomContainerBody.dart';
-import 'package:scouts_system/common%20UI/showTheTextMessage.dart';
-import 'package:scouts_system/view%20model/seasonsGetDataFirestore.dart';
-import 'package:scouts_system/view%20model/studentsGetDataFirestore.dart';
-import 'package:scouts_system/common%20UI/CustomWidgetMethods.dart';
+import 'package:scouts_system/common_ui/circular_progress.dart';
+import 'package:scouts_system/common_ui/custom_container_students.dart';
+import 'package:scouts_system/common_ui/empty_message.dart';
+import 'package:scouts_system/view_model/students.dart';
 
-class StudentListInSeasonsPage extends StatelessWidget {
-  StudentListInSeasonsPage();
+class StudentSeasonsPage extends StatelessWidget {
+  List<dynamic> studentsDocIds;
+  StudentSeasonsPage({required this.studentsDocIds});
 
   Widget build(BuildContext context) {
-    List<dynamic> listOfStudentsInSeason =
-        context
-            .watch<SeasonsGetDataFirestore>()
-            .seasonsListOfDataStudent;
+    StudentsLogic provider = context.watch<StudentsLogic>();
+    if (provider.specificStudents.isEmpty &&
+        provider.stateOfSpecificFetching != StateOfSpecificStudents.loaded) {
+      provider.preparingSpecificStudents(studentsDocIds: studentsDocIds);
+      return CircularProgress();
+    } else
+      return buildScaffold(provider);
+  }
 
-    List<dynamic> listOfAllStudents =
-        context
-            .watch<StudentsGetDataFirestore>()
-            .StudentsListOfData;
-
-    List<int> IndexesOfStudentsData = [];
-
-    for (int i = 0; i < listOfAllStudents.length; i++) {
-      if (listOfStudentsInSeason.contains(listOfAllStudents[i]["docId"]))
-        IndexesOfStudentsData.add(i);
-    }
+  Scaffold buildScaffold(StudentsLogic provider) {
     return Scaffold(
-        appBar: AppBar(backgroundColor: customColor()),
-        body: listOfStudentsInSeason.length == 0
-            ? buildShowMessage("student"):
-        ListView.separated(
-          itemCount: IndexesOfStudentsData.length,
-          separatorBuilder: (BuildContext context, int index) =>
-          const Divider(),
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              title: buildTheItemOfTheList(
-                  listOfAllStudents[IndexesOfStudentsData[index]],
-                  index),
-            );
-          },
-        ));
+        appBar: AppBar(),
+        body: provider.specificStudents.isEmpty
+            ? emptyMessage("student")
+            : buildListView(provider));
+  }
+
+  ListView buildListView(StudentsLogic provider) {
+    return ListView.separated(
+      itemCount: provider.specificStudents.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+      itemBuilder: (BuildContext context, int index) {
+        return buildListTile(provider, index);
+      },
+    );
+  }
+
+  ListTile buildListTile(StudentsLogic provider, int index) {
+    return ListTile(
+      title: buildTheItemOfTheList(provider.specificStudents[index], index),
+    );
   }
 
   SafeArea buildTheItemOfTheList(var model, int index) {
     return SafeArea(
       child: InkWell(
         onTap: () async {},
-        child: CustomContainerBody(model: model, index: index, text: "student"),
+        child: CustomContainerStudents(modelStudents: model, index: index),
       ),
     );
   }
