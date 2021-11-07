23b058847aba7ddda1cd4bd3e4d53de6c02230d8
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-07 15:22:28 +0200

    add seasons button

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:scouts_system/view/events/events_screen.dart';
+import 'package:scouts_system/view/seasons/seasons_screen.dart';
 import 'package:scouts_system/view/students/students_screen.dart';
 import 'common_ui/primary_button.dart';
 
@@ -17,8 +18,10 @@ class HomeScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              PrimaryButton(moveToPage: StudentsPage(), text: "students"),
-              PrimaryButton(moveToPage: EventsPage(), text: "events"),
+              PrimaryButton(moveToPage: StudentsPage(), text: "Students"),
+              PrimaryButton(moveToPage: EventsPage(), text: "Events"),
+              PrimaryButton(moveToPage: SeasonsPage(), text: "Seasons"),
+
             ],
           ),
         ),
