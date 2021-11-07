4817e29b292a29d85a5f93614800148f36863966
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-11-07 15:28:58 +0200

    refactoring the code and make it more readable

--- lib/view/seasons/students_and_events_buttons.dart
+++ lib/view/seasons/students_and_events_buttons.dart
@@ -2,48 +2,50 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
-import 'package:scouts_system/common%20UI/CustomWidgetMethods.dart';
+import 'package:scouts_system/view/seasons/students_items.dart';
 
-import 'package:scouts_system/view/seasons/eventsSeasonList.dart';
-import 'package:scouts_system/view/seasons/studentsSeasonList.dart';
+import 'events_items.dart';
 
 class TwoButtonInSeason extends StatelessWidget {
+  final List<dynamic> studentsDocId;
+  final List<dynamic> eventsDocId;
+  TwoButtonInSeason({required this.eventsDocId, required this.studentsDocId});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: customColor()),
-      body: Column(
-        children: [
-          Container(
-              width: double.infinity,
-              child: TextButton(
-                child: Text(
-                  "events",
-                  style: TextStyle(fontSize: 15, color: Colors.black),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) =>EventsSeasonList() ));
-                },
-              )),
-          Container(
-              width: double.infinity,
-              child: TextButton(
-                child: Text(
-                  "students",
-                  style: TextStyle(fontSize: 15, color: Colors.black),
-                ),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => StudentListInSeasonsPage()));
-                },
-              )),
-        ],
-      ),
+      appBar: AppBar(),
+      body: buildColumn(context),
+    );
+  }
+
+  Column buildColumn(BuildContext context) {
+    return Column(
+      children: [
+        buildContainer(
+            context, "events", EventsSeasonList(eventsDocIds: eventsDocId)),
+        buildContainer(context, "students",
+            StudentSeasonsPage(studentsDocIds: studentsDocId))
+      ],
+    );
+  }
+
+  Container buildContainer(BuildContext context, String text, Widget page) {
+    return Container(
+        width: double.infinity,
+        child: TextButton(
+          child: buildText(text),
+          onPressed: () => onPressed(context, page),
+        ));
+  }
+
+  onPressed(BuildContext context, Widget page) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+  }
+
+  Text buildText(String text) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 15, color: Colors.black),
     );
   }
 }
