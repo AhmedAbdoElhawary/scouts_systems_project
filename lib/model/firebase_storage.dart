fb528ff60bc6a10f2deb15ad5ae16f0cea73882c
ahmed abdo <ahmedabdoelhawari12345work@gmail.com>
2021-12-14 13:49:56 +0200

    reformat FirebaseStorageImage

--- lib/model/firebase_storage.dart
+++ lib/model/firebase_storage.dart
@@ -1,17 +1,13 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart' as Path;
-import 'package:scouts_system/common_ui/toast_show.dart';
 
-class FirebaseStorageImage{
+class FirebaseStorageImage {
   Future<void> deleteImageFromStorage(String imageFileUrl) async {
     String fileUrl = Uri.decodeFull(Path.basename(imageFileUrl))
-        .replaceAll(new RegExp(r'(\?alt).*'), '');
+        .replaceAll(RegExp(r'(\?alt).*'), '');
 
     final Reference firebaseStorageRef =
-    FirebaseStorage.instance.ref().child(fileUrl);
-    await firebaseStorageRef
-        .delete()
-        .then((value){})
-        .catchError((e) {ToastShow().redToast(e);});
+        FirebaseStorage.instance.ref().child(fileUrl);
+    await firebaseStorageRef.delete().then((value) {});
   }
 }
